e365c58c9748fb79f8bc9f215925c35a77f09aa1
Vladimir Gornyy <112384479+The0d4mar@users.noreply.github.com>
2024-01-23 11:22:09 +0300

    modal dz2 in lec3

--- 05-lection3/02-modal/index.js
+++ 05-lection3/02-modal/index.js
@@ -2,6 +2,7 @@
     const close = document.getElementById('modal-close');
     const modal = document.getElementById('modal');
     const modalOpen = document.getElementById('modal-open');
+    const blackout = document.querySelector('.modalBlackout');
 
     if (!close || !modalOpen || !modal) {
         return;
@@ -9,11 +10,13 @@
 
     modalOpen.addEventListener('click', () => {
         modal.style.display = 'block';
+        blackout.classList.toggle('modalBlock_view');
         document.body.overflow = 'hidden';
     });
 
     close.addEventListener('click', () => {
         modal.style.display = 'none';
+        blackout.classList.remove('modalBlock_view');
         document.body.overflow = 'initial';
     });
 })();
